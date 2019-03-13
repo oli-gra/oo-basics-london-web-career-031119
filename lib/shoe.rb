d09d9be316c1be7d6654c54d244f44bda6a7dcdd
Oliver <oliver.grant@gmail.com>
2019-03-13 14:18:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,14 @@
 
 class Shoe
 
-attr_accessor :brand
+attr_accessor :brand, :color, :size, :material, :condition
 
 def initialize (brand)
   @brand = brand
+end
+
+def cobble
+  puts ""
 end
 
 end
