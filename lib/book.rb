6c8f4ea627de32bf206ce834fb029e4a2d52b82c
Oliver <oliver.grant@gmail.com>
2019-03-13 14:16:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,13 @@
 class Book
   
-  attr_accessor :title, :author, :page_count, :genre, :turn_page
+  attr_accessor :title, :author, :page_count, :genre
 
 def initialize (title)
   @title = title
 end
+
+def turn_page
+  puts ""
 
 end
 
